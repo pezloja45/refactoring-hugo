7711693661221b003f9c04bc5540ea5598522bc2
Hugo <martin.romero.hugo@iescamas.es>
2024-01-17 17:09:27 +0100

    Ejercicio 4 añadido

--- src/refactoring/Ejercicio4.java
+++ src/refactoring/Ejercicio4.java
@@ -1,10 +1,10 @@
-package refactoring;
-
-public class Ejercicio4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+														package refactoring;
+												   import java.util.Scanner;
+												   public class Ejercicio4 {
+public static void main(String[] args) {Scanner sc = new Scanner(System.in);
+System.out.println("Escribe una frase:");
+String LoCuRa = sc.nextLine();String NoLoCuRa = "";
+int numLetras = LoCuRa.length();
+			 for (int nUmErItO = numLetras - 1; nUmErItO >= 0; nUmErItO--) {
+				 	   NoLoCuRa = NoLoCuRa  + "" + LoCuRa.charAt(nUmErItO);}
+			 					  System.out.println(NoLoCuRa);sc.close();}}
